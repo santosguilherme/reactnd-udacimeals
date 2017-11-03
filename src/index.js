74cebe78597a8a9265634bccf86a26e115894ffb
Guilherme Santos <sntguilherme@gmail.com>
2017-11-02 22:24:55 -0200

    redux integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import {createStore} from 'redux';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers';
 
+import './index.css';
+
+
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-    <App/>,
+    <App store={store}/>,
     document.getElementById('root')
 );
 registerServiceWorker();
